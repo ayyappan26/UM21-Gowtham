3553684ba2b42206541d411e9b4086da0884c8b2
Admin <Admin@LAPTOP-0SM5THGP>
2021-09-07 09:21:12 +0530

    First Commit

---
+++ FourthJavaProject/src/com/ultramain/um21/employeeview/EmployeeView.java
@@ -0,0 +1,10 @@
+package com.ultramain.um21.employeeview;
+
+public class EmployeeView {
+public static void main(String[] args){
+	
+	 EmployeeBo empbo = new EmployeeBo();
+	   empbo.empsystem();
+
+}
+}
